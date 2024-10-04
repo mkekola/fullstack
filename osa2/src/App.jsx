c899c171cf5d386ea3d769db2850c8a0a60d2125
Maria Kekola <maria.kekola@helsinki.fi>
2024-10-05 00:44:35 +0300

    Tehtävä 2.1

---
+++ osa2/src/App.jsx
@@ -0,0 +1,78 @@
+const Course = (props) => {
+  const course = props.course
+  return (
+    <div>
+      <Header course={course.name}> </Header>
+      <Content parts={course.parts}> </Content>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let totalExercises = 0
+
+  for (let i = 0; i < props.parts.length; i++) {
+    totalExercises += props.parts[i].exercises
+  }
+
+  return <p>Number of exercises {totalExercises}</p>
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
